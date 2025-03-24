019d45f807352e1e9f49af4e9a1054a0d1cfa7f4
walt <2963975+waltiam@users.noreply.github.com>
2025-03-24 17:00:23 +0000

    🎨 pugging

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,8 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+  .about
+    h1 This is an about page written in
+      span(style="color:red;")  PUG
+      | .
 </template>
 
 <style>
